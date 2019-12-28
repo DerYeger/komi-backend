c330ce7742f86400198c909da8cf8091b57293fc
Jan Müller <janmueller3698@gmail.com>
2019-12-28 23:52:17 +0100

    WIP lobby joining

--- src/main/kotlin/eu/yeger/komi/backend/Configuration.kt
+++ src/main/kotlin/eu/yeger/komi/backend/Configuration.kt
@@ -20,47 +20,92 @@ class WSConfig : WebSocketConfigurer {
     }
 }
 
-data class User(val id: Long)
-data class Lobby(val id: Long) {
-    val players: MutableList<User> = mutableListOf()
-}
+data class User(val id: Long, val name: String, @Transient val lobby: Lobby? = null)
+
+data class Lobby(val id: Long, val name: String, val players: List<User>)
 
 data class Message(val type: String, val data: Any)
 
 class LobbyHandler : TextWebSocketHandler() {
-    private val sessions = HashMap<WebSocketSession, User>()
-    private val lobbies = HashSet<Lobby>()
+    private val userMap = HashMap<WebSocketSession, User>()
+    private val lobbyMap = HashMap<Long, Lobby>()
 
     private val uids = AtomicLong(0)
     private val lids = AtomicLong(0)
 
-    override fun afterConnectionEstablished(session: WebSocketSession) {
-        sessions[session] = User(uids.getAndIncrement())
-        session.send(Message("lobbies", lobbies))
-    }
-
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
-        sessions.remove(session)
-    }
+        val user = userMap.remove(session)!!
+        val lobby = user.lobby
 
-    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
-        val json = ObjectMapper().readTree(message.payload)
-        when (json.get("type").asText()) {
-            "leaveLobby" -> leaveLobby(session)
-            "createLobby" -> createLobby()
+        if (lobby !== null) {
+            lobbyMap[lobby.id] = lobby.copy(players = lobby.players - user)
         }
     }
 
-    private fun leaveLobby(session: WebSocketSession) {
-        sessions -= session
+    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        try {
+            val json = ObjectMapper().readTree(message.payload)
+            if (!json.has("type")) return
+            when (json.get("type").asText()) {
+                "join" -> session.join(json.get("data").asText())
+                "leave" -> session.leave()
+                "createLobby" -> session.createLobby(json.get("data").asText())
+                "joinLobby" -> session.joinLobby(json.get("data").asLong())
+                "leaveLobby" -> session.leaveLobby(json.get("data").asLong())
+            }
+        } catch (e: Exception) {
+            session.close(CloseStatus.SERVER_ERROR)
+        }
     }
 
-    private fun createLobby() {
-        lobbies += Lobby(lids.getAndIncrement())
-        broadcast(Message("lobbies", lobbies))
+    private fun WebSocketSession.join(name: String) {
+        userMap[this] = User(uids.getAndIncrement(), name)
+        this.send(Message("lobbies", lobbyMap.values))
+    }
+
+    private fun WebSocketSession.leave() {
+        userMap -= this
+    }
+
+    private fun WebSocketSession.createLobby(name: String) {
+        if (userMap[this]?.lobby !== null) {
+            return this.close(CloseStatus.SERVER_ERROR)
+        }
+
+        val lobby = Lobby(lids.getAndIncrement(), name, listOf())
+        lobbyMap[lobby.id] = lobby
+        broadcast(Message("lobbies", lobbyMap.values))
+        joinLobby(lobby.id)
+    }
+
+    private fun WebSocketSession.joinLobby(id: Long) {
+        val user = userMap[this]!!
+        val lobby = lobbyMap[id]
+
+        if (user.lobby !== null || lobby === null || lobby.players.size > 1) {
+            return this.close(CloseStatus.SERVER_ERROR)
+        }
+
+        userMap[this] = user.copy(lobby = lobby)
+        lobbyMap[id] = lobby.copy(players = lobby.players + user)
+        broadcast(Message("lobbies", lobbyMap.values))
+    }
+
+    private fun WebSocketSession.leaveLobby(id: Long) {
+        val user = userMap[this]!!
+        val lobby = lobbyMap[id]
+
+        if (user.lobby === null || lobby === null) {
+            return this.close(CloseStatus.SERVER_ERROR)
+        }
+
+        userMap[this] = user.copy(lobby = null)
+        lobbyMap[id] = lobby.copy(players = lobby.players - user)
+        broadcast(Message("lobbies", lobbyMap.values))
+        send(Message("joinedLobby", lobby))
     }
 
     private fun WebSocketSession.send(message: Message) = sendMessage(TextMessage(jacksonObjectMapper().writeValueAsString(message)))
 
-    private fun broadcast(message: Message) = sessions.forEach { it.key.send(message) }
+    private fun broadcast(message: Message) = userMap.forEach { it.key.send(message) }
 }
